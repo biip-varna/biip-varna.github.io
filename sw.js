cd32332a4b6fc23e638078b0ad721b871fee94c8
biip-varna <biip.office@protonmail.com>
2026-03-28 17:57:40 +0100

    Revert "chore: bump asset versions to 3990fac"
    This reverts commit 16b79df296feb9063e55d548f98fb697f0d09cac.

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    Provides offline fallback for homepage and key pages.
    ================================================================ */
 
-var CACHE_VERSION = 'biip-3990fac';
+var CACHE_VERSION = 'biip-v4';
 var STATIC_CACHE  = CACHE_VERSION + '-static';
 var PAGES_CACHE   = CACHE_VERSION + '-pages';
 
